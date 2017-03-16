0aec9c0e2908012114c8d6dadc0ce55f3c4f71ed
Tomoki Yamashita <tomorrowkey@gmail.com>
2017-03-16 22:02:10 +0900

    Add log group and log stream args

--- lib/cwlog_tail.rb
+++ lib/cwlog_tail.rb
@@ -7,10 +7,10 @@ module CwlogTail
   client = CwClient.new
   options = Options.new(ARGV)
 
-  log_group_name = PecoSelector.select_from(client.log_group_names).first
+  log_group_name = options.log_group || PecoSelector.select_from(client.log_group_names).first
   exit 0 unless log_group_name
 
-  log_stream_name = PecoSelector.select_from(client.log_streams(log_group_name, options.page_count).map{|log_stream|
+  log_stream_name = options.log_stream || PecoSelector.select_from(client.log_streams(log_group_name, options.page_count).map{|log_stream|
     ["#{Time.at(log_stream.last_event_timestamp / 1000)} #{log_stream.log_stream_name}", log_stream]
   }).first&.log_stream_name
   exit 0 unless log_stream_name
